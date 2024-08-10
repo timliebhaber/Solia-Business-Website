b1a7d56a46427c0fb9f483ffbe7004a2be20f016
Tim Liebhaber <19033267-timliebhaber@users.noreply.gitlab.com>
2024-08-10 13:42:56 +0200

    Header and Logo

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+import AboutMe from './components/AboutMe.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Solia Logo" class="logo" src="./assets/Logo Border.png" width="125" height="125" />
+
+    <div class="wrapper">
+      <img alt="Solia Solutions" class="logo" src="./assets/Solia Solutions Color.png" width="259" height="113" />
+    </div>
+  </header>
+
+  <main>
+    <AboutMe />
+
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/AboutMe.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import WelcomeItem from './Header.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Leistungen</template>
+    <ul>
+      <li>KI Beratung</li>
+      <li>IT-Security</li>
+      <li>Webdesign</li>
+      <li>Anwendungsentwicklung</li>
+    </ul>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Referenzen</template>
+    
+    Hier stehen meine Referenzen
+    
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Über mich</template>
+
+    Ich bin ein studierter Informatiker mit Schwerpunkt in künstlicher Intelligenz. Ich bin interessiert an Neuroinformatik, KI und Blockchain-Technologie, Web Entwicklung und Software Engineering.
+    Mehr über mich findest du auf meinem
+    <a href="https://www.linkedin.com/in/tim-liebhaber/" target="_blank" rel="noopener">LinkedIn</a>.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Kontakt</template>
+
+    Kontaktieren Sie mich hier:
+
+    Kontaktfeld
+
+  </WelcomeItem>
+</template>
