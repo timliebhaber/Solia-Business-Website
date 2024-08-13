22d0ba1b94e904f458fdac757f0728d8f7447858
timliebhaber <timliebhaber@gmail.com>
2024-08-13 23:19:17 +0200

    feat: added referenzen field

---
+++ src/components/Referenzen.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="referenzen">
+    <img src="https://via.placeholder.com/150" alt="Image 1">
+    <img src="https://via.placeholder.com/150" alt="Image 2">
+    <img src="https://via.placeholder.com/150" alt="Image 3">
+    <img src="https://via.placeholder.com/150" alt="Image 4">
+  </div>
+</template>
+
+<style scoped>
+#referenzen {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin: 20px;
+}
+</style>
