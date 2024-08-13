f85d2d5016ecc0b137c667eb74548a598cd8109d
timliebhaber <timliebhaber@gmail.com>
2024-08-13 23:46:46 +0200

    feat: added about me section

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,3 +1,16 @@
+<template>
+    <div id="über">
+    <h3>Über mich</h3>
+
+    <p>
+    Ich bin studierter Informatiker mit Schwerpunkt in künstlicher Intelligenz. Ich bin interessiert an Neuroinformatik, KI und Blockchain-Technologie, Web Entwicklung und Software Engineering.
+    Mehr über mich findest du auf meinem <a href="https://www.linkedin.com/in/tim-liebhaber/" target="_blank" rel="noopener">LinkedIn</a>.
+    </p> 
+
+    </div>
+
+</template>
+
 <script setup lang="ts">
 import DocumentationIcon from './icons/IconDocumentation.vue'
 import ToolingIcon from './icons/IconTooling.vue'
@@ -5,51 +18,3 @@ import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
 </script>
-
-<template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Leistungen</template>
-    <ul>
-      <li>KI Beratung</li>
-      <li>IT-Security</li>
-      <li>Webdesign</li>
-      <li>Anwendungsentwicklung</li>
-    </ul>
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Referenzen</template>
-    
-    Hier stehen meine Referenzen
-    
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Über mich</template>
-
-    Ich bin ein studierter Informatiker mit Schwerpunkt in künstlicher Intelligenz. Ich bin interessiert an Neuroinformatik, KI und Blockchain-Technologie, Web Entwicklung und Software Engineering.
-    Mehr über mich findest du auf meinem
-    <a href="https://www.linkedin.com/in/tim-liebhaber/" target="_blank" rel="noopener">LinkedIn</a>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Kontakt</template>
-
-    Kontaktieren Sie mich hier:
-
-    Kontaktfeld
-
-  </WelcomeItem>
-</template>
