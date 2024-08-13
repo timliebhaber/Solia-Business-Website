dd55334fb55cae5970c3c7c5e2a3577e9887447c
Tim Liebhaber <19033267-timliebhaber@users.noreply.gitlab.com>
2024-08-13 15:38:30 +0200

    App vue

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,78 @@
 <script setup lang="ts">
-import Header from './components/Header.vue'
+import Willkommen from './components/Willkommen.vue';
 import AboutMe from './components/AboutMe.vue'
+import Kontakt from './components/Kontakt.vue';
+import Leistungen from './components/Leistungen.vue';
+
 </script>
 
 <template>
   <header>
-    <img alt="Solia Logo" class="logo" src="./assets/Logo Border.png" width="125" height="125" />
-
-    <div class="wrapper">
-      <img alt="Solia Solutions" class="logo" src="./assets/Solia Solutions Color.png" width="259" height="113" />
+    <div class="logo-container">
+            <img src="./assets/Logo White.png" alt="Logo" class="logo-small">
+        
+        <nav class="nav-links">
+            <a href="#home">Home</a>
+            <a href="#leistungen">Leistungen</a>
+            <a href="#über">Über mich</a>
+            <a href="#kontakt">Kontakt</a>
+        </nav>
     </div>
   </header>
 
   <main>
+    <Willkommen />
+    <Leistungen/>
     <AboutMe />
-
+    <div id="kontakt">
+        <Kontakt />
+    </div>
+   
+    
   </main>
 </template>
 
 <style scoped>
+
 header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    color: white;
 }
+
+.logo-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.logo-small {
+    width: 30px;
+    height: 30px;
+    margin-right: 20px;
+}
+
+.logo-large {
+    width: 60px;
+    height: 40px;
+    transform: scaleX(1.5); /* Slightly horizontally bigger */
+    margin-top: 5px;
+}
+
+#kontakt {
+  display: flex;
+  justify-content: center; /* Horizontally centers the container */
+}
+
+.nav-links a {
+    color: white;
+    padding: 10px 20px;
+    text-decoration: none;
+    margin: 0 15px;
+    font-size: 18px;
+    transition: color 0.3s ease;
+}
+
 </style>
