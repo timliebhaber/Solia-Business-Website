918a987bd74337634fb9d9b9ca2b36dd128b9812
timliebhaber <timliebhaber@gmail.com>
2024-08-13 23:47:58 +0200

    feat: added leistungen section

--- src/components/Leistungen.vue
+++ src/components/Leistungen.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-  <div class="greetings">
+  <div id="leistungen">
+    <h3>Leistungen</h3>
     <ul>
       <li>KI Beratung</li>
       <li>IT-Security</li>
@@ -14,26 +15,6 @@
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
