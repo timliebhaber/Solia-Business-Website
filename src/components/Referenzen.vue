adacffd6f760e87bbaab8b370c368058f57ef1c5
timliebhaber <timliebhaber@gmail.com>
2024-08-14 23:37:35 +0200

    feat: upgreaded referenzen section

--- src/components/Referenzen.vue
+++ src/components/Referenzen.vue
@@ -7,23 +7,25 @@
     <a href="https://khettabi.com"><img src="../assets/Logo-Badro-white.svg" alt="Khettabi" height = 50px></a>
     <img src="../assets/DHRW_ENG_WHITE.png" alt="DHRW Engineering" height = 60px>
     <img src="../assets/Logo_Akad_Normal.png" alt="DHRW Akademie" height = 60px>
-    <img src="../assets/Logo Sirali.png" alt="Sirali" height = 60px>
+    <img src="../assets/Logo Sirali.png" alt="Sirali" height = 55px>
 
   </div>
 </template>
 
 <style scoped>
 #referenzen {
-  width:100%;
+  width: 100%;
   background-color: white;  
   display: flex;
-    justify-content: space-around;
-    align-items: center;
-    padding: 10px;
+  justify-content: space-around;
+  align-items: center;
+  padding: 10px;
 }
 
 img {
-  /* From https://css.glass */
-  filter: brightness(0) ;
+  filter: brightness(0);
 }
+
+
+
 </style>
