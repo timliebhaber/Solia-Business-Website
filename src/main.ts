bc058e7d595b09bae35c1a5944d95bc15964388e
Tim Liebhaber <19033267-timliebhaber@users.noreply.gitlab.com>
2024-08-19 02:03:43 +0200

    chore: Renaming Sections and fixing unprecize code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+library.add(faPlay);
+
+
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app')
+
