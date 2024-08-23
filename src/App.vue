6ce012c1c2ec1ca999ad40713bc245beee55aa53
timliebhaber <timliebhaber@gmail.com>
2024-08-23 16:14:06 +0200

    chore: redid hero structure and added company logo

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,7 @@ import Hero from './components/HeroSection.vue';
 
 <template>
   <header>
-    <!-- TODO: include navbar into hero section  -->
-     <!-- TODO: Navbar for sizes that overlap code -> Slide-out Menu (Hamburger) -->
-    <div class="navbar">
-        <img src="./assets/Logo White.png" alt="Logo" class="logo-small">
-        <nav class="nav-links">
-            <a href="#home">Home</a>
-            <a href="#leistungen">Leistungen</a>
-            <a href="#über">Über mich</a>
-            <a href="#kontakt">Kontakt</a>
-        </nav>
-    </div>
+    
   </header>
 
   <main>
@@ -34,54 +24,6 @@ import Hero from './components/HeroSection.vue';
 
 <style scoped lang="scss">
 
-.navbar {
-    background-color: transparent;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 15rem;
-    padding-top: 4.5rem;
-    height: 8vh;
-}
 
-.logo-small {
-    width: 30px;
-    height: 30px;
-    margin-right: 20px;
-}
-
-.nav-links a {
-    color: white;
-    padding: 10px 20px;
-    text-decoration: none;
-    font-family: 'Haas', sans-serif;
-    font-weight: bold;
-    font-size: 18px;
-    transition: background-color 0.3s ease-in-out;
-    border-radius: 5px;
-}
-
-.nav-links a:hover {
-    background-color: #ffffff23;
-    -webkit-backdrop-filter: blur(10px);
-  backdrop-filter: blur(10px);
-  border-radius: 8px;
-  box-shadow: 0 4px 90px rgba(0,0,0,0.1);
-  overflow: hidden;
-  &:before {
-    content: "";
-    position: absolute;
-    z-index: 2;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 2px;
-    background: rgb(255,255,255);
-    background: linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255, 255, 255, 0.473) 34%, rgba(255,255,255,1) 89%, rgba(255,255,255,0) 100%);
-    opacity: 0.3;
-    filter: blur(.5px);
-    mix-blend-mode: hard-light;
-  }
-}
 
 </style>
