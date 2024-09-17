b39bdb06b5b1e661611ed69f45b2ed9599c99a0d
Tim Liebhaber <timliebhaber@gmail.com>
2024-09-17 23:46:48 +0200

    refactor: Remove unnecessary code and improve text in SliderTest.vue

--- src/components/SliderTest.vue
+++ src/components/SliderTest.vue
@@ -167,7 +167,7 @@ swiper.on('slideChangeTransitionEnd', function () {
           </div>
 
           <p class="news__txt">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...
+            Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...
           </p>
 
           <div class="news__img">
