ab216dfa3e7b6d069f264882351c6873ffe365c9
timliebhaber <timliebhaber@gmail.com>
2024-12-03 23:42:39 +0100

    Refactor: Update axios dependency to version 1.7.8 and comment out Kontakt component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import Kontakt from './components/KontaktSection.vue';
-import Referenzen from './components/ReferenzenSection.vue';
 import Hero from './components/HeroSection.vue';
 import LeistungsCardsInvert from './components/LeistungsCardsInvert.vue';
 import AboutMeCard from './components/AboutMeCard.vue';
@@ -18,7 +17,7 @@ import ReferenzenSectionSlider from './components/ReferenzenSectionSlider.vue';
     <LeistungsCardsInvert/>
     <ReferenzenSectionSlider />
     <AboutMeCard />
-    <Kontakt />
+    <!-- <Kontakt /> -->
   </main>
 </template>
 
